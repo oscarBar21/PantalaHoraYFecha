b4089bbc48a114e51999ce2f403cdc6f2c348508
oscar <oscarbarragan31@gmail.com>
2021-03-02 10:00:53 +0000

    02 pantalla hora y fecha almacena internamente una fecha

---
+++ PantallaHoraYFecha.java
@@ -0,0 +1,15 @@
+public class PantallaHoraYFecha{
+    private int horas;
+    private int minutos;
+    private int dia;
+    private int mes;
+    private int ano;
+    
+    public PantallaHoraYFecha(){
+       horas = 00;
+       minutos = 00;
+       dia = 1;
+       mes = 1;
+       ano = 1;
+    }
+}

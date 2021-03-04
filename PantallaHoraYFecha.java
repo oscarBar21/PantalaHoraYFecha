c38542f3225d0f8985e67f17a7e36d2aa0442159
oscar <oscarbarragan31@gmail.com>
2021-03-04 12:43:37 +0000

    03 cree tre metodos uno para que muestre la fecha y la hora otro para avanzar y el ultrimo para poner yo la fecha que quiera

--- PantallaHoraYFecha.java
+++ PantallaHoraYFecha.java
@@ -12,4 +12,35 @@ public class PantallaHoraYFecha{
        mes = new PantallaDosDigitos(1, 13);
        ano = new PantallaDosDigitos(1, 99);
     }
+    
+    public String getFechaYHora() {
+        String fechaYHora;
+        fechaYHora = (horas.getTextoDeLaPantalla() + ":" + minutos.getTextoDeLaPantalla() + " " + dia.getTextoDeLaPantalla() + "-" + mes.getTextoDeLaPantalla() + "-" + ano.getTextoDeLaPantalla());
+        return fechaYHora;
+    }
+    
+    public void avanzarMinuto(){
+        minutos.incrementaValorAlmacenado();
+        
+        if (minutos.getValorAlmacenado() == 0) {
+            horas.incrementaValorAlmacenado();
+            if (horas.getValorAlmacenado() == 0) {
+                dia.incrementaValorAlmacenado();
+                if (dia.getValorAlmacenado() == 1) {
+                    mes.incrementaValorAlmacenado();
+                    if (mes.getValorAlmacenado() == 1) {
+                        ano.incrementaValorAlmacenado();
+                    }
+                }
+            }
+        }
+    }
+    
+    public void fijarFechaYHora(int minuto, int hora, int dias, int meses, int anos){
+        horas.setValorAlmacenado(hora);
+        minutos.setValorAlmacenado(minuto);
+        dia.setValorAlmacenado(dias);
+        mes.setValorAlmacenado(meses);
+        ano.setValorAlmacenado(anos);
+    }
 }

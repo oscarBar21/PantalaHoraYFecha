57d16c0d41208a7005fbd1a95cf9c2f25b5a0fa0
oscar <oscarbarragan31@gmail.com>
2021-03-04 12:08:11 +0000

    02-1.2 pantallahorayfecha ahora esta relacionada con pantalladosdigitos pero sigue almacenando una hora interna

--- PantallaHoraYFecha.java
+++ PantallaHoraYFecha.java
@@ -9,7 +9,7 @@ public class PantallaHoraYFecha{
        horas = new PantallaDosDigitos(0, 24);
        minutos = new PantallaDosDigitos(0, 60);
        dia = new PantallaDosDigitos(1, 31);
-       mes = new PantallaDosDigitos(1, 12);
+       mes = new PantallaDosDigitos(1, 13);
        ano = new PantallaDosDigitos(1, 99);
     }
 }
